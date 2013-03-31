7f06d19e32806f69e97e30f7e81dc5c7bf4db5b3
Nathan Naze <nnaze@google.com>
2013-03-30 23:10:04 -0400

    Add function to find an identifier for a JsDoc comment.

--- parser_test.py
+++ parser_test.py
@@ -3,13 +3,18 @@ import unittest
 
 class ParserTestCase(unittest.TestCase):
 
-  def testColor(self):
+  def testFindDocComments(self):
     matches = list(parser.FindJsDocComments(_TEST_SCRIPT))
     self.assertEquals(1, len(matches))
 
     match = matches[0]
     self.assertEquals(10, match.start())
     self.assertEquals(34, match.end())
+
+  def testFindIdentifier(self):
+    match = list(parser.FindJsDocComments(_TEST_SCRIPT))[0]
+    identifier_match = parser.FindIdentiferForComment(match)
+    self.assertEquals('goog.bar.baz', identifier_match.group())
 
 
 _TEST_SCRIPT = """\
