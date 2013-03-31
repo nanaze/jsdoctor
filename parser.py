2c8489838f745656a4489b9d573ec0aa96849361
Nathan Naze <nanaze@gmail.com>
2013-03-30 23:10:04 -0400

    Add function to find an identifier for a JsDoc comment.

--- parser.py
+++ parser.py
@@ -3,3 +3,9 @@ import re
 def FindJsDocComments(script):
   return re.finditer('/\*\*.*?\*/', script, re.DOTALL)
 
+def FindIdentiferForComment(comment_match):
+  end_index = comment_match.end()
+  identifier_regex = re.compile('(?:\w+\s*\.\s*)*\w+')
+  script = comment_match.string
+  return identifier_regex.search(script, comment_match.end())
+
