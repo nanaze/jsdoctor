4cf7058583b5e4e35356c9dd51a6a34f992c5761
Nathan Naze <nanaze@gmail.com>
2013-04-28 19:06:36 -0400

    Parse HTML elements, add HRs to descriptons, put code in code blocks.

--- generator.py
+++ generator.py
@@ -12,7 +12,7 @@ def GenerateDocs(namespace_map):
     content = doc.documentElement.toxml('utf-8')
     yield filepath, content
 
-def _ParseDocumentFragment(content):
+def _ProcessString(content):
   return html5lib.parseFragment(content, treebuilder='dom')
 
 def _MakeTextNode(content):
@@ -55,20 +55,21 @@ def _AddSymbolDescriptions(node_list, symbols):
   for symbol in symbols:
     node_list.append(_MakeElement('h3', symbol.identifier))
     for section in symbol.comment.description_sections:
-      elem = _ParseDocumentFragment(section)
+      elem = _ProcessString(section)
       p = _MakeElement('p')
       node_list.append(p)
       p.appendChild(elem)
+    node_list.append(_MakeElement('hr'))
 
 def _MakeLink(text, href):
   a = _MakeElement('a', text)
   a.setAttribute('href', href)
   return a
 
-def _MakeFunctionSummary(name, function):
-  container = _MakeElement('p')
-  container.appendChild(_MakeLink(name, '#' + name))
-  container.appendChild(_MakeTextNode('('))
+def _MakeFunctionCodeElement(name, function):
+  code = _MakeElement('code')
+  code.appendChild(_MakeLink(name, '#' + name))
+  code.appendChild(_MakeTextNode('('))
 
   param_flags = filter(lambda flag: flag.name == '@param',
                        function.comment.flags)
@@ -76,33 +77,40 @@ def _MakeFunctionSummary(name, function):
   for index, flag in enumerate(param_flags):
     name, type, _ = flags.ParseParameterDescription(flag.text)
 
-    container.appendChild(_MakeTextNode('{%s}' % type))
-    container.appendChild(_MakeTextNode(' '))
-    container.appendChild(_MakeTextNode(name))
+    code.appendChild(_MakeTextNode('{%s}' % type))
+    code.appendChild(_MakeTextNode(' '))
+    code.appendChild(_MakeTextNode(name))
 
     # If this is not the last param, add a comma
     last_index = (len(param_flags) - 1)
     if index != last_index:
-      container.appendChild(_MakeTextNode(', '))
+      code.appendChild(_MakeTextNode(', '))
 
-  container.appendChild(_MakeTextNode(')'))
-  
+  code.appendChild(_MakeTextNode(')'))
+
   return_flags = filter(lambda flag: flag.name == '@return',
-                        function.comment.flags)
+                        function.comment.flags)      
 
   assert(len(return_flags) <= 1, 'There should not be more than one @return flag.')
 
   if return_flags:
     return_flag = return_flags[0]
-    container.appendChild(_MakeTextNode(' : '))
+    code.appendChild(_MakeTextNode(' : '))
     type, _ = flags.ParseReturnDescription(return_flag.text)
-    container.appendChild(_MakeTextNode('{%s}' % type))
+    code.appendChild(_MakeTextNode('{%s}' % type))
+
+  return code
+  
+def _MakeFunctionSummary(name, function):
+  container = _MakeElement('p')
+  code = _MakeFunctionCodeElement(name, function)
+  container.appendChild(code)  
 
   if function.comment.description_sections:
     desc = function.comment.description_sections[0]
     container.appendChild(_MakeElement('br'))
-    container.appendChild(_ParseDocumentFragment(desc))
-    
+    container.appendChild(_ProcessString(desc))
+
   return container
   
 
@@ -145,7 +153,7 @@ def _GenerateContent(namespace, symbols):
       node_list.append(_MakeFunctionSummary(method.property, method))
 
   if static_functions:
-    node_list.append(_MakeElement('h2', 'Static methods'))
+    node_list.append(_MakeElement('h2', 'Static method summary'))
     for function in static_functions:
       node_list.append(_MakeFunctionSummary(function.identifier, function))      
 
